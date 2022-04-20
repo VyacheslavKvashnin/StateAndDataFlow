bd9bdcff630bec469cae7456973b2cee1d81fe8e
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-20 22:52:56 +0500

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Вячеслав Квашнин on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
